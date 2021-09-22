25bd477a3a597abe7ca89f33dca112cc3ddd7e56
JeffersonSoares99 <jefferson99soares@gmail.com>
2021-09-22 20:07:46 -0300

    Ações dos botões no java

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -4,16 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Variaveis globais que representam os objetos no xml
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //abrindo a minha activity
         setContentView(R.layout.activity_main);
+
+        //declarei as variáveis do java para apresentar para as variáveis xml
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtSenha = findViewById(R.id.edtSenha);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        //ação do botão entrar
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Bem vindo ao sistema!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 
+
     public void sairSistema(View view) {
-        finish();
+        finish(); //finalizando a activity
     }
 }
