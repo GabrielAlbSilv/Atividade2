542fd0a7909b8c3e409dcddd16120ce2983563a1
GabrielAlbSilv <gabrielalbuquerquemoura@gmai.com>
2021-10-02 14:07:31 -0300

    nova commit

--- app/src/main/java/br/com/etecia/myapp/cadastro.java
+++ app/src/main/java/br/com/etecia/myapp/cadastro.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Menu_Activity extends AppCompatActivity {
+public class cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_layout);
+        setContentView(R.layout.layout_cadastro);
     }
 }
